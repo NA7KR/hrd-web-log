52afe8945828a03da8404cc430ac233c199bc7a1
NA7KR <kevin@na7kr.us>
2013-05-12 23:40:51 -0700

    osversion

--- counter.php
+++ counter.php
@@ -113,6 +113,51 @@ class OS_BR{
     }
 }
 
+$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+$user_os        =   getOS();
+
+function getOS() {
+
+    global $user_agent;
+
+    $os_platform    =   "Unknown OS Platform";
+
+    $os_array       =   array(
+                            '/windows nt 6.2/i'     =>  'Windows 8',
+                            '/windows nt 6.1/i'     =>  'Windows 7',
+                            '/windows nt 6.0/i'     =>  'Windows Vista',
+                            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                            '/windows nt 5.1/i'     =>  'Windows XP',
+                            '/windows xp/i'         =>  'Windows XP',
+                            '/windows nt 5.0/i'     =>  'Windows 2000',
+                            '/windows me/i'         =>  'Windows ME',
+                            '/win98/i'              =>  'Windows 98',
+                            '/win95/i'              =>  'Windows 95',
+                            '/win16/i'              =>  'Windows 3.11',
+                            '/macintosh|mac os x/i' =>  'Mac OS X',
+                            '/mac_powerpc/i'        =>  'Mac OS 9',
+                            '/linux/i'              =>  'Linux',
+                            '/ubuntu/i'             =>  'Ubuntu',
+                            '/iphone/i'             =>  'iPhone',
+                            '/ipod/i'               =>  'iPod',
+                            '/ipad/i'               =>  'iPad',
+                            '/android/i'            =>  'Android',
+                            '/blackberry/i'         =>  'BlackBerry',
+                            '/webos/i'              =>  'Mobile'
+                        );
+
+    foreach ($os_array as $regex => $value) {
+
+        if (preg_match($regex, $user_agent)) {
+            $os_platform    =   $value;
+        }
+
+    }
+
+    return $os_platform;
+
+}
+
 // using
 // create an new instant of OS_BR class
 $obj = new OS_BR();
@@ -128,7 +173,7 @@ $version = $obj->showInfo('version');
 $os =  $obj->showInfo('os');
 
 
-$SQL = " INSERT INTO `visit` (`visitorIP`  ,`visitURL`, `browser`  ,`version`,`os`) VALUES ( '$ipAddress'  , '$visitURL', '$browser', '$version', '$os')";
+$SQL = " INSERT INTO `visit` (`visitorIP`  ,`visitURL`, `browser`  ,`version`,`os`, `osversion`) VALUES ( '$ipAddress'  , '$visitURL', '$browser', '$version', '$os','$user_os' )";
 
 $exec = mysql_query($SQL) or die(mysql_error()); 
 ?>
