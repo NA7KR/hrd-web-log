6428ef802b7ad53ed46ac11340b0fe472a4e71e7
NA7KR <kevin@na7kr.us>
2013-12-01 21:14:34 -0800

    file upload

--- welcome.php
+++ welcome.php
@@ -4,41 +4,42 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	<title>Welcome Admin</title>
+	<style>
+		.sucess
+		{
+			color:#088A08;
+		}
+		.error
+		{
+			color:red;
+		}
+	</style>";
 </head>
-
 <body>
-<h1>Welcome Admin <?php echo $login_session; ?></h1> 
-
-<h2><a href="logout.php">Sign Out</a></h2>
-
-
-
 <html> 
-<form action="welcome.php" method="post" enctype="multipart/form-data"> 
-<label for="file">Filename:</label> 
-<input type="file" name="file" id="file"><br> 
-<input type="submit" name="submit" value="Submit"> 
-</form> 
+	<h1>Welcome Admin <?php echo $login_session; ?></h1> 
+	<h2><a href="logout.php">Sign Out</a></h2>
+
+    <form enctype="multipart/form-data" class="Txt_upload" method='POST'  action='welcome.php'>
+            <label for="file">Enter upload here:</label>
+            <input type='file' name='file'/>
+            <input type='submit' name='submit' value="upload here"/>
+        </form>
+    </body>
+</html>
 
 <?php
-	if (isset($_POST['Submit']))
+    if(isset($_FILES['file']))
 	{
-		echo "<style>";
-		echo ".sucess";
-		echo "{color:#088A08;}";
-		echo ".error";
-		echo "{color:red;}";
-		echo "</style>";
-
 		$upload_exts = "";
 		$file_exts = array("jpg", "bmp", "jpeg", "gif", "png");
 		$upload_exts = end(explode(".", $_FILES["file"]["name"]));
 		if ((($_FILES["file"]["type"] == "image/gif")
-		|| ($_FILES["file"]["type"] == "image/jpeg")
-		|| ($_FILES["file"]["type"] == "image/png")
-		|| ($_FILES["file"]["type"] == "image/pjpeg"))
-		&& ($_FILES["file"]["size"] < 2000000)
-		&& in_array($upload_exts, $file_exts))
+			|| ($_FILES["file"]["type"] == "image/jpeg")
+			|| ($_FILES["file"]["type"] == "image/png")
+			|| ($_FILES["file"]["type"] == "image/pjpeg"))
+			&& ($_FILES["file"]["size"] < 2000000)
+			&& in_array($upload_exts, $file_exts))
 		{
 			if ($_FILES["file"]["error"] > 0)
 			{
@@ -47,9 +48,6 @@
 			else
 			{
 				echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-				echo "Type: " . $_FILES["file"]["type"] . "<br>";
-				echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-				echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 				// Enter your path to upload file here
 				if (file_exists("Awards/" .
 				$_FILES["file"]["name"]))
@@ -62,6 +60,7 @@
 				"Awards/" . $_FILES["file"]["name"]);
 				echo "<div class='sucess'>"."Stored in: " .
 				"Awards/" . $_FILES["file"]["name"]."</div>";
+				
 			}
 		}
 		}
@@ -71,8 +70,5 @@
 		}
 	}
 ?>
-
- 
-
 </body>
 </html>
