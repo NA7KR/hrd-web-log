eb95fef6afa2ed9dd04fc71c5558bd0f03f78767
NA7KR <kevin@na7kr.us>
2013-12-05 16:21:35 -0800

    file upload work in progess

--- welcome.php
+++ welcome.php
@@ -1,6 +1,44 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<script type="text/javascript">
+	function makeEnable1()
+	{	
+		var x=document.getElementById("mySelect1")
+		x.disabled=false
+		var x=document.getElementById("mySelect2")
+		x.disabled=true
+		var x=document.getElementById("mySelect3")
+		x.disabled=true
+	}
+	function makeEnable2()
+	{	
+		var x=document.getElementById("mySelect1")
+		x.disabled=false
+		var x=document.getElementById("mySelect2")
+		x.disabled=false
+		var x=document.getElementById("mySelect3")
+		x.disabled=true
+	}
+	function makeEnable3()
+	{
+		var x=document.getElementById("mySelect1")
+		x.disabled=true
+		var x=document.getElementById("mySelect2")
+		x.disabled=true
+		var x=document.getElementById("mySelect3")
+		x.disabled=false
+	}
+	function onLoad()
+	{
+		var x=document.getElementById("mySelect1")
+		x.disabled=true
+		var x=document.getElementById("mySelect2")
+		x.disabled=true
+		var x=document.getElementById("mySelect3")
+		x.disabled=true
+	}
+</script>
 	<meta name="keywords" content="Ham Radio NA7KR">
 	<meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
 	<meta name="description" content="<?php echo $myCall ?> Ham Radio LogBook"> 
@@ -13,37 +51,67 @@
 		include('lock.php');
 		include('style.php');
 	?>
-	<title><?php echo $myCall ?> Ham Radio LogBook Upload / config</title> 
+<title><?php echo $myCall ?> Ham Radio LogBook Upload / config</title> 
 </head>
 
 <body onload="onLoad();" span class="background1">
-<?php include_once("analyticstracking.php") ?>
-    <div class="auto-style1"> Hello welcome Admin Page <span class="auto-style3"><br>
-        </span><span class="auto-style4">My Call is <?php echo $myCall ?></span><br>
-    </div>
-	
+    <div class="auto-style1"> Hello welcome Admin Page..... 
+		<span class="auto-style3"><br></span>
+		<span class="auto-style4">My Call is <?php echo $myCall ?></span><br>
+    </div>	
 <html> 
 	<h1>Welcome Admin <?php echo $login_session; ?></h1> 
 	<h2><a href="logout.php">Sign Out</a></h2>
-
+	
+	<br><br>
     <form enctype="multipart/form-data" class="Txt_upload" method='POST'  action='welcome.php'>
-            <label for="file">Enter upload here Awards:            </label><br>
-            <input type='file' name='file'/><br><br>
-			
-			<label for="filecard">Enter upload here Paper Card:     </label><br>
-            <input type='file' name='filecard'/><br><br>
-			
-			<label for="fileback">Enter upload here Paper Card Back:</label><br>
-            <input type='file' name='fileback'/><br><br>
-			
+			<input type="radio" value='1' name="Log" onclick="makeEnable1()" > Upload Awards
+			<input type="radio" value='2' name="Log" onclick="makeEnable2()" > Upload Paper
+			<input type="radio" value='3' name="Log" onclick="makeEnable2()" > Upload Paper Back
+			<input type="radio" value='4' name="Log" onclick="makeEnable3()" > Look contact number
+			<br>
+			<table width="30%" border="0" cellpadding="0" cellspacing="0"  >
+				<tr valign="top">
+					<td style="border-width : 0px;"><label>Enter Contact Number :</label></td>
+					<td style="border-width : 0px;"><input type="text"  id='mySelect2' name="contactno" ></td>
+				</tr>
+				<tr valign="top">
+					<td style="border-width : 0px;"><label>Enter Callsign :</label></td>
+					<td style="border-width : 0px;"><input type="text"  id='mySelect3' name="callsign" ></td>
+				</tr>
+				<tr valign="top">
+					<td style="border-width : 0px;"><label>Select file :</label></td>
+					<td style="border-width : 0px;"><input type='file' id='mySelect1' name='file' /></td>
+				</tr>
+			</table>
+			<br>
             <input type='submit' name='submit' value="upload here"/>
         </form>
     </body>
 </html>
 
 <?php
+	if ($debug == "true")
+		{
+			echo "<pre>";
+			print_r($_POST);
+			echo "</pre>";
+			echo "<pre>";
+			print_r($_FILES);
+			echo "</pre>";
+		}
     if(isset($_FILES['file']))
-	{
+	{	
+		$LOG = $_POST['Log'];
+		if ($LOG == 1)
+		{
+			$target_path = "Awards/";
+		}
+		else
+		{
+			$target_path = "cards/0-999/";
+		}
+		
 		$upload_exts = "";
 		$file_exts = array("jpg", "bmp", "jpeg", "gif", "png");
 		$upload_exts = end(explode(".", $_FILES["file"]["name"]));
@@ -62,18 +130,14 @@
 			{
 				echo "Upload: " . $_FILES["file"]["name"] . "<br>";
 				// Enter your path to upload file here
-				if (file_exists("Awards/" .
-				$_FILES["file"]["name"]))
+				if (file_exists($target_path . $_FILES["file"]["name"]))
 			{
 				echo "<div class='error'>"."(".$_FILES["file"]["name"].")". " already exists. "."</div>";
 			}
 			else
 			{
-				move_uploaded_file($_FILES["file"]["tmp_name"],
-				"Awards/" . $_FILES["file"]["name"]);
-				echo "<div class='sucess'>"."Stored in: " .
-				"Awards/" . $_FILES["file"]["name"]."</div>";
-				
+				move_uploaded_file($_FILES["file"]["tmp_name"], $target_path . $_FILES["file"]["name"]);
+				echo "<div class='sucess'>"."Stored in: " . $target_path . $_FILES["file"]["name"]."</div>";	
 			}
 		}
 		}
