a1b90698a119d58f05e6119576f2a3825fb4ae22
NA7KR <kevin@na7kr.us>
2014-01-09 13:00:04 -0800

    new files

---
+++ backend.php
@@ -0,0 +1,117 @@
+<?php
+/*************************************************************************
+* 			NA7KR Log Program 
+**************************************************************************
+
+**************************************************************************
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 2 of the License, or
+*   (at your option) any later version.
+*
+**************************************************************************/
+
+#################################################
+# QRZ.com callsign lookup 
+#################################################
+function qrzcom_interface($callsign) 
+{
+    $lookup = "<a href='http://www.qrz.com/db/$callsign'>$callsign</a>";
+    return ($lookup);
+}
+
+#################################################
+# QRZ.com callsign lookup 
+#################################################
+function qsl_worked()
+{
+    include (__DIR__ . '/../config.php');
+    require_once('Lookup.class.php');
+    $db = new Db();
+    $id_lookup = $db->query("SELECT DISTINCT `COL_CALL` FROM NA7KR.TABLE_HRD_CONTACTS_V01 WHERE 1");
+    foreach ($id_lookup as $row):
+    $QSLWORKED .=$row['COL_CALL'] . ',';
+        if ($debug == "true") {
+            echo $QSLWORKED;
+        }
+    endforeach;
+    return ($QSLWORKED);
+    
+} 
+
+#################################################
+# read DB for oprion view
+#################################################
+function select() {
+    include (__DIR__ . '/../config.php');
+    require_once('Lookup.class.php');
+    $Select_txt ='<FORM name ="form1" method ="post" action ="index.php">'. PHP_EOL;
+    $db = new Db();
+    $id_lookup = $db->query("SELECT Select_TXT, Select_Name FROM $dbnameWEB.$tbSelect ORDER BY `Select_TXT` ");
+    foreach ($id_lookup as $row):
+
+        if ($row['Select_Name'] == "callsign_lookup") {
+            $Select_txt .='<input type="radio" value=' . $row['Select_Name'] . ' name="Log" > ' . $row['Select_TXT']. PHP_EOL;
+        } else {
+            $Select_txt .='<input type="radio" value=' . $row['Select_Name'] . ' name="Log"  > ' . $row['Select_TXT']. PHP_EOL;
+        }
+    endforeach;
+    $Select_txt .='<br><div><P style="text-align: center">'. PHP_EOL;
+    $Select_txt .= '<Input type = "Submit" Name = "Submit1" VALUE = "Submit"></p></div></FORM><BR>'. PHP_EOL;
+    return $Select_txt;
+}
+
+
+#################################################
+# build include files
+#################################################
+function buildfiles($Key) {
+    include (__DIR__ . '/../config.php');
+    require_once('Lookup.class.php');
+    $db = new Db();
+        $id_lookup = $db->row("SELECT Select_File FROM $dbnameWEB.$tbSelect WHERE Select_Name = '$Key'");
+    return $id_lookup['Select_File'];
+}
+
+#################################################
+# SQL safe
+#################################################
+function safe($value) {
+    return mysql_real_escape_string($value);
+}
+
+#################################################
+# Make Grid
+# $i mod 2 is checking to see if the row number is odd or even odd rows are colored differently than even rows to create a datagrid look and feel
+#################################################
+function grid_style($i) {
+    if (($i % 2) != 0) {
+        $style = "<tr bgcolor='#5e5eff'>";
+        return $style;
+    } else {
+        $style = "<tr bgcolor='#FFC600'>";
+        return $style;
+    }
+}
+
+#################################################
+# 
+# 
+#################################################
+function footer($i) {    
+?>
+<div class="c1">
+<span class="auto-style5">
+    <?php    
+        date_default_timezone_set('America/Los_Angeles');
+        echo "Was last modified: " . date("F d Y H:i", filemtime($i));
+        require_once("backend.php");
+    ?>
+</span>
+</div>
+<br><br>
+
+<div style="display:none;"><?php echo qsl_worked(); ?></div>
+<?php 
+}
+?>
---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+/* * ***********************************************************************
+ * 			NA7KR Log Program 
+ * *************************************************************************
+
+ * *************************************************************************
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ * ************************************************************************ */
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="Admin/css/style.css" />
+        <title>
+            <?php
+            include_once (__DIR__ . '/../config.php');
+            require("backend.php");
+            include "counter.php";
+            echo $myCall
+            ?> 
+            Ham Radio LogBook</title> 
+        <meta name="keywords" content="Ham Radio NA7KR">
+        <meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
+        <meta name="description" content="<?php echo $myCall ?> Ham Radio LogBook"> 
+        <meta http-equiv="content-type" content="text/html;charset=UTF-8"> 
+        <meta name="revisit-after" content="1 days">
+        <META NAME="ROBOTS" CONTENT="INDEX, FOLLOW">
+    </head>
+
+    <body onload="onLoad();" class="background1">
+        <?php include_once("analyticstracking.php") ?>
+        <div class="auto-style1"> Hello welcome my log book at reads from Ham Radio Deluxe log.. <span class="auto-style3"><br>
+            </span><span class="auto-style4">My Call is <?php echo qrzcom_interface($myCall) ?></span><br>
+        </div>
+        <br><br>
+        <?php
+        if (isset($_POST['Submit1'])) {
+            $LOG = safe($_POST['Log']);
+            include_once buildfiles($LOG);
+            
+        } else {
+            echo select();
+            ?>
+            <div><p style="text-align: center"> Only band and modes in your log shows</p>
+                <p style="text-align: center"> This is a addon and not any way a part of HRD.</p>
+                <div><P style="text-align: center"><img  alt="HRD" src="HRD_logo.jpg"></p></div>
+                <p style="text-align: center"> Please contact <a href="mailto:support@na7kr.us"><span class="auto-style2">support@na7kr.us</span></a> and not HRD.</p></div>
+
+            <p>
+            <div class="c1">
+                <span class="auto-style5">
+                    <a href="http://validator.w3.org/check?uri=referer">
+                        <img class="c4"  src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88">
+                    </a>
+                </span>
+            </div>
+            <br><br>
+            <div class="c1">
+                <span class="auto-style5">
+                    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                        <img class="c4" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!">
+                    </a>
+                </span>
+            </div>
+            <br><br>
+        <?php
+        $phpfile = __FILE__ ;
+        footer($phpfile);
+        }
+        
+        ?>
+       
+</html>
+   
+       
