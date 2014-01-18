cfed2226d3a34846eb715e8bd0e128ef55f7a261
NA7KR <kevin@na7kr.us>
2014-01-18 12:54:58 -0800

    Full

--- select_full.php
+++ select_full.php
@@ -1,4 +1,5 @@
 <?php
+
 /* * ***********************************************************************
  * 			NA7KR Log Program 
  * *************************************************************************
@@ -11,17 +12,78 @@
  *
  * ************************************************************************ */
 include_once (__DIR__ . '/../config.php');
-    require_once('db.class.php');
-    require_once("backend.php");
-    $db = new Db();
-   
-    $i = 0; //style counter
-    $counter = 0;
-    $filePath = "/Awards";
-    $id_lookup = $db->query("SELECT date(`COL_TIME_OFF`)AS`Date` ,`COL_CALL`AS`CallSign`,`COL_MODE`AS`Mode` ,"
-        . " `COL_BAND`AS`Band` ,`COL_GRIDSQUARE`AS`Grid` ,`COL_COUNTRY`AS`Country` ,"
-        . "`COL_STATE`AS`State` ,`COL_QTH`AS`QTH` FROM $dbnameHRD.$tbHRD WHERE COL_BAND LIKE '%$Band%' AND "
-        . " COL_MODE LIKE '%$Mode%' ORDER BY $dbnameHRD.$tbHRD.`COL_PRIMARY_KEY` DESC");
+require_once('db.class.php');
+require_once("backend.php");
+$db = new Db();
+$i = 0; //style counter
+$x = 0; //
+$FileNoGroup = 0;
+$find = '.jpg';
+$fileMutiply = 1000;
+$counter = 0;
+if (isset($_POST['Submit1'])) {
+    $LOG = \filter_input(\INPUT_POST, 'Log', \FILTER_SANITIZE_STRING);
+    $QTY = \filter_input(\INPUT_POST, 'Qty', \FILTER_SANITIZE_STRING);
+    $SUBMIT = \filter_input(\INPUT_POST, 'Submit', \FILTER_SANITIZE_STRING);
+    $CALL_SEARCH = \filter_input(\INPUT_POST, 'Call_Search', \FILTER_SANITIZE_STRING);
+    $BAND =  \filter_input(\INPUT_POST, 'Band', \FILTER_SANITIZE_STRING);
+    $MODE = \filter_input(\INPUT_POST, 'Mode', \FILTER_SANITIZE_STRING);
+    $STATE = \filter_input(\INPUT_POST, 'State', \FILTER_SANITIZE_STRING);
+    $COUNTRY = \filter_input(\INPUT_POST, 'Country', \FILTER_SANITIZE_STRING);
+    $INPUT = \filter_input(\INPUT_POST, 'optionlist', \FILTER_SANITIZE_STRING);
+    include_once buildfiles($LOG);
+    $data .= '<input type="hidden" name="Log" value=' . $LOG . '>' . PHP_EOL;
+    $data .= '<input type="hidden" name="Submit" value="true">' . PHP_EOL;
+ 
+}
+$query= "SELECT date(`COL_TIME_OFF`)AS`Date` , \n"
+        . " `COL_CALL`AS`CallSign`, \n"
+        . " `COL_MODE`AS`Mode` , \n"
+        . " `COL_BAND`AS`Band` , \n"
+        . " `COL_GRIDSQUARE`AS`Grid` , \n"
+        . "`COL_COUNTRY`AS`Country` , \n"
+        . "`COL_STATE`AS`State` ,"
+        . "`COL_QTH`AS`QTH` FROM $dbnameHRD.$tbHRD \n"
+        . " __REPLACE__ \n"
+        . "ORDER BY $dbnameHRD.$tbHRD.`COL_PRIMARY_KEY` \n"
+        . "DESC ";  
+if ($SUBMIT == "true") {
+    if ($INPUT == "input1") {
+        $BAND = safe("%" . $BAND . "%");
+        $query = str_replace("__REPLACE__", "where COL_BAND like $BAND ", $query);
+    }
+    elseif ($INPUT == "input2") {
+        $MODE = safe("%" . $MODE . "%");
+        $MODE = str_replace("USB", "SSB", $MODE);
+        $MODE = str_replace("LSB", "SSB", $MODE);
+        $query = str_replace("__REPLACE__", "where COL_MODE like $MODE ", $query);
+    } 
+    elseif ($INPUT == "input3"){
+        $CALL_SEARCH = safe("%" . $CALL_SEARCH . "%");
+        $query = str_replace("__REPLACE__", "where COL_CALL like $CALL_SEARCH ", $query);
+    }
+    elseif ($INPUT == "input4"){
+        $STATE = safe("%" . $STATE . "%");
+        $query = str_replace("__REPLACE__", "where COL_STATE like $STATE ", $query);
+    }
+    elseif ($INPUT == "input5"){
+        $COUNTRY = safe("%" . $COUNTRY . "%");
+        $query = str_replace("__REPLACE__", "where COL_COUNTRY like $COUNTRY ", $query);
+    }
+    elseif ($INPUT == "input6"){
+        $query = str_replace("__REPLACE__", " ", $query);
+    }
+    else
+    {
+       $query = str_replace("__REPLACE__", " ", $query); 
+    }
+    if ($QTY == "All") {
+        $id_lookup = $db->query("$query ");
+    } else {
+        $query = str_replace("DESC", "DESC Limit $QTY ", $query);
+        $id_lookup = $db->query("$query");
+    }
+    $id_lookup = $db->query($query);
     
     $data = "<table border='0' align='center'><tbody><tr>"
             . "<th>Date</th>"
@@ -49,6 +111,24 @@ include_once (__DIR__ . '/../config.php');
     endforeach;
     $data .= "</table>" . PHP_EOL;
     $data .= "<p style='text-align: center'><BR> Counter " . $counter . "</p><BR>". PHP_EOL;
+    $data .=OptionList(false,false,false,false,false,false). PHP_EOL;
+}
+else {
+    $data ='<table width=600 class="center2">'. PHP_EOL;
+    $data .='<tr><td>'. PHP_EOL;
+    $data .=OptionList(true,true,false,true,true,true). PHP_EOL;
+    $data .=band() . PHP_EOL;
+    $data .=mode() . PHP_EOL;
+    $data .=OptionState() . PHP_EOL;
+    $data .=OptionCountry() . PHP_EOL;
+    $data .='</td></tr>' . PHP_EOL;
+    $data .='</table>' . PHP_EOL;
+    $data .='<div class="c1">' . PHP_EOL;
+    $data .='<span class="auto-style5">' . PHP_EOL;
+    $data .='Select from drop down the amount of QLS would like to return<br>' . PHP_EOL;
+    $data .=OptionQty() . PHP_EOL;
+    $data .='<Input type = "Submit" Name = "Submit1" VALUE = "Submit"></span></div></FORM><BR>' . PHP_EOL;  
+}
     echo $data;
     $phpfile = __FILE__ ;
     footer($phpfile);
