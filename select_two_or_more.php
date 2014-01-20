23681bf8005ebefa5cb7260760386cf6f5962d14
NA7KR <kevin@na7kr.us>
2014-01-20 12:03:26 -0800

    multiple

--- select_two_or_more.php
+++ select_two_or_more.php
@@ -23,13 +23,49 @@ require_once("backend.php");
 $db = new Db();
 $i = 0; //style counter
 $x = 0; //
-$counter = 0;
-$query = "00";
-$id_lookup = $db->query($query);
+$FileNoGroup = 0;
+$find = '.jpg';
+$fileMutiply = 1000;
+if (isset($_POST['Submit1'])) {
+    $SUBMIT = \filter_input(\INPUT_POST, 'Submit', \FILTER_SANITIZE_STRING);
+    $MODE = \filter_input(\INPUT_POST, 'Mode', \FILTER_SANITIZE_STRING);
+    $STATE = \filter_input(\INPUT_POST, 'State', \FILTER_SANITIZE_STRING);
+    $COUNTRY = \filter_input(\INPUT_POST, 'Country', \FILTER_SANITIZE_STRING);
+    $INPUT = \filter_input(\INPUT_POST, 'optionlist', \FILTER_SANITIZE_STRING);
+    include_once buildfiles($LOG);
+    $data .= '<input type="hidden" name="Log" value=' . $LOG . '>' . PHP_EOL;
+    $data .= '<input type="hidden" name="Submit" value="true">' . PHP_EOL;
+}
+$query = "Select count(*) as `Count`, \n"
+        . " COL_CALL from(SELECT distinct COL_CALL, \n"
+            . " COL_BAND, \n"
+            . " COL_MODE, \n"
+            . " count(*) \n"
+            . " FROM NA7KR.`TABLE_HRD_CONTACTS_V01` \n"
+            . " __REPLACE__ \n"
+            . " group by 1,2,3 ) as CALLSIGN \n"
+        . " group by 2 HAVING Count > 1 \n"
+        . " ORDER BY Count DESC";
+
+
+if ($SUBMIT == "true") {
+    if ($INPUT == "input_mode") {
+        $MODE = safe("%" . $MODE . "%");
+        $MODE = str_replace("USB", "SSB", $MODE);
+        $MODE = str_replace("LSB", "SSB", $MODE);
+        $query = str_replace("__REPLACE__", " where COL_MODE like $MODE ", $query);
+    } elseif ($INPUT == "input_none") {
+        $query = str_replace("__REPLACE__", " ", $query);
+    } else {
+        $query = str_replace("__REPLACE__", " ", $query);
+    }
+
+    $id_lookup = $db->query($query);
     $data = "<table border='0' align='center'><tbody><tr><th>Country</th></tr><tr bgcolor='#5e5eff'>" . PHP_EOL;
     foreach ($id_lookup as $row): {
             $fileName = $row['File'];
-            $data .= "<td>" . $row['Countrys Worked'] . "</td>" . PHP_EOL . grid_style($i) . PHP_EOL;
+            $data .= "<td>" . $row['Count'] . "</td>" . PHP_EOL ;
+            $data .= "<td>" . $row['COL_CALL'] . "</td>" . PHP_EOL . grid_style($i) . PHP_EOL;
             $counter++;
             $i++;
             unset($row); // break the reference with the last element
@@ -37,7 +73,20 @@ $id_lookup = $db->query($query);
     endforeach;
     $data .= "</table>" . PHP_EOL;
     $data .= "<p style='text-align: center'><BR> Counter " . $counter . "</p><BR>" . PHP_EOL;
-    $data .=OptionList(false, false, false, false, false, false) . PHP_EOL; 
+    $data .=OptionList(false, false, false, false, false, false) . PHP_EOL;
+} else {
+    $data = '<table width=600 class="center2">' . PHP_EOL;
+    $data .='<tr><td>' . PHP_EOL;
+    $data .=OptionList(false, true, false, false, false, true) . PHP_EOL;
+    $data .=mode() . PHP_EOL;
+    $data .='</td></tr>' . PHP_EOL;
+    $data .='</table>' . PHP_EOL;
+    $data .='<div class="c1">' . PHP_EOL;
+    $data .='<span class="auto-style5">' . PHP_EOL;
+    $data .='<Input type = "Submit" Name = "Submit1" VALUE = "Submit"></span></div></FORM><BR>' . PHP_EOL;
+}
+//$data .=$query;
 echo $data;
 $phpfile = __FILE__;
 footer($phpfile);
+?>
